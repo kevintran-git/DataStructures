71512e8b8bd515ab4f32e37c49d0e94c0baa626d
kevintran-git <kinzerfest@gmail.com>
2020-09-30 12:39:59 -0500

    Added junit

---
+++ src/test/java/queues/WaitLineTest.java
@@ -0,0 +1,20 @@
+package queues;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WaitLineTest {
+
+    @Test
+    void simulate() {
+    }
+
+    @Test
+    void displayResults() {
+    }
+
+    @Test
+    void reset() {
+    }
+}
